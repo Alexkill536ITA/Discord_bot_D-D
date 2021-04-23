3ac42c22e257f8c684b94ba11bc8c463c0451bcd
Alexkill536ITA <manzo.alizzi@libero.it>
2021-04-23 11:04:59 +0200

    avatar system custom v1 copleate

--- commands/avatar_pg.js
+++ commands/avatar_pg.js
@@ -25,7 +25,77 @@ module.exports = {
         let myRole = message.guild.roles.cache.find(role => role.name === config.role_base);
         if (message.member.roles.cache.some(r => config.role_base.includes(r.name)) || message.author.id == config.owner) {
             var colrs_set = clor_gen.rand_Color();
-
+            if (args[0] == "show" || args[0] == "-v") {
+                if (args[1] && args[1].length == 24) {
+                    var cursor = await get_Scheda_pg(args[1]);
+                    cursor.then(function (result) {
+                        if (result != null) {
+                            var js_result = JSON.stringify(result);
+                            js_result = JSON.parse(js_result);
+                            let member = message.guild.members.cache.get(js_result['Nome_Discord']);
+                            if (js_result['Avatar_pg'] == "Non Assegnata" || js_result['Avatar_pg'] == undefined) {
+                                var avatar = member.user.displayAvatarURL();
+                            } else {
+                                var avatar = js_result['Avatar_pg'];
+                            }
+                            Container = new Discord.MessageEmbed();
+                            Container.setColor(colrs_set)
+                                .setTitle('Avatar PG di: ' + js_result['Nome_PG'])
+                                .setTimestamp()
+                                .setFooter("Data", message.author.displayAvatarURL())
+                                .setImage(avatar);
+                            message.channel.send(Container);
+                        } else {
+                            Container.setColor([255, 0, 0])
+                                .setAuthor(`Richiesta di: ${message.author.username}`)
+                                .setTitle('Errore Scheda non trovata');
+                            message.channel.send(Container);
+                        }
+                    });
+                } else {
+                    emit_print(message);
+                }
+            } else if (args[0] == "set" || args[0] == "-s") {
+                if (args[1]) {
+                    var cursor = await get_Scheda_pg(args[1]);
+                    cursor.then(function (result) {
+                        if (result != null) {
+                            var js_result = JSON.stringify(result);
+                            js_result = JSON.parse(js_result);
+                            if (message.author.id != js_result['Nome_Discord']) {
+                                Container.setColor([255, 0, 0])
+                                    .setAuthor(`Utente non valido: ` + message.author.username)
+                                    .setTitle('Non puoi spacciarti per un altro');
+                                message.channel.send(Container);
+                                return 1
+                            }
+                            if (validURL(args[1])) {
+                                Container = new Discord.MessageEmbed();
+                                Container.setColor(colrs_set)
+                                    .setTitle('Avatar PG di: ' + js_result['Nome_PG'])
+                                    .setTimestamp()
+                                    .setFooter("Data", message.author.displayAvatarURL())
+                                    .setImage(avatar);
+                                message.channel.send(Container);
+                            } else {
+                                Container.setColor([255, 0, 0])
+                                    .setAuthor(`Richiesta di: ${message.author.username}`)
+                                    .setTitle('Errore URL Non Valido');
+                                message.channel.send(Container);
+                            }
+                        } else {
+                            Container.setColor([255, 0, 0])
+                                .setAuthor(`Richiesta di: ${message.author.username}`)
+                                .setTitle('Errore Scheda non trovata');
+                            message.channel.send(Container);
+                        }
+                    });
+                } else {
+                    emit_print(message);
+                }
+            } else {
+                emit_print(message);
+            }
         } else {
             Container.setColor([255, 0, 0])
                 .setAuthor(`🚫 Access denied ` + message.author.username + " 🚫")
@@ -33,4 +103,33 @@ module.exports = {
             message.channel.send(Container);
         }
     }
+}
+
+function emit_print(message) {
+    var Container = new Discord.MessageEmbed();
+    Container.setColor([255, 0, 0])
+        .setAuthor(`Comando pgavatar`)
+        .setTitle('Sintassi:\n **' + config.prefix + 'pgavatar** [Opzione][ID_Scheda]');
+    message.channel.send(Container);
+}
+
+async function get_Scheda_pg(id_serach) {
+    var on_sevice_db = await methodDB.open_db();
+    if (on_sevice_db != 1) {
+        methodDB.settab_db("Schede_PG");
+        var cursor = methodDB.serachbyid(id_serach);
+    } else {
+        return 1;
+    }
+    return cursor;
+}
+
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(str);
 }
